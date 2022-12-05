5600001ac7edcea5bfcd77211287c2f0d5edcd8e
Romain Menke <romainmenke@gmail.com>
2022-12-05 08:23:18 +0100

    populatedb

--- cssdb.mjs
+++ cssdb.mjs
@@ -362,6 +362,7 @@ export default [
       "android": "106",
       "chrome": "106",
       "edge": "106",
+      "firefox": "109",
       "ios_saf": "16",
       "op_mob": "72",
       "safari": "16"
